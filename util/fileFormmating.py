ac186a4ff3f3a6a5af9182bbc19fab4cef283c9f
liupan010 <liupan010@ke.com>
2018-10-29 19:52:51 +0800

    initial commit

---
+++ util/fileFormmating.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+import os
+
+class Tool:
+    def __init__(self):
+        pass
+    def test(self):
+        print "Hello World!"
+    def getDir(self, flag = "pwd"):
+        if flag == "pwd":
+            res_path = os.getcwd()
+        if flag == "father":
+            pwd = os.getcwd()
+            res_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + ".")
+        if flag == "grand":
+            pwd = os.getcwd()
+            res_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
+        return res_path
+
+if __name__ == '__main__':
+    T = Tool()
